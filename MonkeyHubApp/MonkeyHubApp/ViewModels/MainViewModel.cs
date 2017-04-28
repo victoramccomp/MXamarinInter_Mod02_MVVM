b3a54f4f0dff2adcd1c5d556cd418ceaf44dac0c
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2017-04-27 21:26:02 -0300

    Binding field first step

--- MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,12 +1,26 @@
-﻿namespace MonkeyHubApp.ViewModels
+﻿using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MonkeyHubApp.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string Descricao { get; set; }
 
         public MainViewModel()
         {
             Descricao = "Olá Mundo! Eu estou aqui!";
+
+            Task.Delay(3000).ContinueWith(t =>
+            {
+                Descricao = "Meu texto mudou!";
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Descricao"));
+                }
+            });
         }
     }
 }
